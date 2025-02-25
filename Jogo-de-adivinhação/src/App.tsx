25f35f6c1356bac94e2f225cd26bc3c5f87a8e7b
Ingrid Lira <gridferreiradoa2109@gmail.com>
2025-02-25 12:21:30 -0300

    Styling main

--- Jogo-de-adivinhação/src/App.tsx
+++ Jogo-de-adivinhação/src/App.tsx
@@ -4,23 +4,23 @@ import { Tip } from "./components/Tip";
 import { Letter } from "./components/Letter";
 
 export default function App() {
-function handleRestartGame(){
-  alert('Reiniciar o jogo')
-}
+  function handleRestartGame() {
+    alert("Reiniciar o jogo");
+  }
 
   return (
     <div className={styles.container}>
       <main>
-      <Header current={5} max={10} onRestart={handleRestartGame}/>
-      <Tip tip="Uma das linguagens de programação mais utilizadas no mundo" />
-      <Letter value="R" />
-      <Letter value="E" />
-      <Letter value="A" />
-      <Letter value="C" />
-      <Letter value="T" />
+        <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Tip tip="Uma das linguagens de programação mais utilizadas no mundo" />
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
       </main>
     </div>
   );
 }
-
-
