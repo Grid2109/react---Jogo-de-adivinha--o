da090071cf034246a7c54c5e7f76fc16ed76a9f7
Ingrid Lira <gridferreiradoa2109@gmail.com>
2025-02-25 12:09:17 -0300

    CREATING LETTER COMPONENT

--- Jogo-de-adivinhação/src/App.tsx
+++ Jogo-de-adivinhação/src/App.tsx
@@ -1,6 +1,7 @@
 import styles from "./App.module.css";
 import { Header } from "./components/Header";
 import { Tip } from "./components/Tip";
+import { Letter } from "./components/Letter";
 
 export default function App() {
 function handleRestartGame(){
@@ -12,6 +13,11 @@ function handleRestartGame(){
       <main>
       <Header current={5} max={10} onRestart={handleRestartGame}/>
       <Tip tip="Uma das linguagens de programação mais utilizadas no mundo" />
+      <Letter value="R" />
+      <Letter value="E" />
+      <Letter value="A" />
+      <Letter value="C" />
+      <Letter value="T" />
       </main>
     </div>
   );
